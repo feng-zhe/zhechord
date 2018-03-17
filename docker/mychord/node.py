d2f1ad3cff3f71131e1d45ff944ae82eb76bf0dc
Henry Feng <henryzhefeng@gmail.com>
2018-03-16 21:47:17 -0400

    finish init_finger_table

--- docker/mychord/node.py
+++ docker/mychord/node.py
@@ -11,35 +11,18 @@ class Node(object):
     '''
     This node represents a node(server) in chord ring.
     '''
-    def __init__(self):
-        self.successor = None
-        self.predecessor = None
-        self.finger_table = ft.FingerTable()
-
-    def calc_id(self, name):
+    def __init__(self, identity):
         '''
-        Calculate the id (SHA1 hash) of name.
+        Initialze:
+
+        self._id:   The identity of this node.
+        self._predecessor:    The predecessor of this node.
+        self._table:    The finger_table of this node.
+
+        Be aware that the successor is the finger[1].node.
 
         Args:
-            name:   a string to be hashed.
-
-        Returns:
-            The SHA1 hashed value which is 20 hex characters.
-
-        Raises:
-            N/A
-        '''
-        m = hashlib.sha1()
-        m.update(name.encode('utf-8'))
-        h = m.hexdigest()
-        return h
-
-    def create_chord(self):
-        '''
-        Create a new chord ring from itself.
-
-        Args:
-            N/A
+            identity:   The identity of this node.
 
         Returns:
             N/A
@@ -47,15 +30,17 @@ class Node(object):
         Raises:
             N/A
         '''
-        # TODO
-        pass
+        self._id = identity
+        self._predecessor = None
+        self._table = ft.FingerTable(identity)
 
-    def join(self, node):
+    def join(self, remote_node):
         '''
-        Join a chord ring.
+        Create/join a chord ring.
 
         Args:
-            node:   A node which is already in the ring.
+            remote_node:   The identity of the node which is already in the ring.
+                    If None, create a new ring.
 
         Returns:
             N/A
@@ -63,15 +48,20 @@ class Node(object):
         Raises:
             N/A
         '''
-        # TODO
-        pass
+        if remote_node:        # join a ring via node
+            self.init_finger_table(remote_node)
+            self.update_others()
+        else:       # the first one in the ring
+            self._predecessor = self._id
+            for i in range(1, ct.RING_SIZE_BIT+1):
+                self._table.set_node(1, self._id)
 
     def find_successor(self, identity):
         '''
         Find the successor of this node.
 
         Args:
-            node:   A node which is already in the ring.
+            identity:   The identity of the object.
 
         Returns:
             The identity of the successor in hex.
@@ -114,15 +104,45 @@ class Node(object):
         # TODO
         pass
 
-    def initial_finger_table(self):
+    def init_finger_table(self, remote_node):
         '''
-        Initialize the calling node's finger table.
+        Initialize the calling node's finger table by the remote_node.
 
         Args:
-            N/A
+            remote_node:    The remote node id.
 
         Returns:
             N/A
+
+        Raises:
+            N/A
+        '''
+        successor = self.remote_find_successor(remote_node, 
+                                self._table.get_start(1))
+        self._table.set_node(1, successor)
+        self._predecessor = self.remote_get_predecessor(successor)
+        self.remote_set_predecessor(successor, self._id)
+        for i in range(1, ct.RING_SIZE_BIT):
+            start = self._table.get_start(i+1)
+            fnode = self._table.get_node(i)
+            if self._in_range(start, self._id, fnode):
+                self._table.set_node(i+1, fnode)
+            else:
+                remote_succ = self.remote_find_successor(remote_node, start)
+                self._table.set_node(i+1, remote_succ)
+            
+    def _in_range(self, node, start, end):
+        '''
+        Test whether the node is in [start, end).
+        It will handle the wrap around problem.
+
+        Args:
+            node:   The node to be tested.
+            start:  The range start, included.
+            end:    The range end, excluded.
+
+        Returns:
+            True if in range. False otherwise.
 
         Raises:
             N/A
@@ -154,15 +174,16 @@ class Node(object):
         # TODO
         pass
 
-    def stablize(self):
+    def remote_find_successor(self, remote_node, identity):
         '''
-        Periodically verify n’s immediate successor, and tell the successor about n
+        Ask the remote node to find the successor of this node.
 
         Args:
-            N/A
+            remote_node:    The remote node id.
+            identity:       The identity to loop up.
 
         Returns:
-            N/A
+            The id of the successor.
 
         Raises:
             N/A
@@ -170,15 +191,15 @@ class Node(object):
         # TODO
         pass
 
-    def notify(self, node):
+    def remote_get_predecessor(self, remote_node):
         '''
-        The node thinks it might be our predecessor
+        Get the predecessor of the remote_node.
 
         Args:
-            node:   the node notifying this node.
+            remote_node:    The remote node id.
 
         Returns:
-            N/A
+            The id of the predecessor.
 
         Raises:
             N/A
@@ -186,12 +207,44 @@ class Node(object):
         # TODO
         pass
 
-    def fix_fingers(self):
+    def get_predecessor(self):
         '''
-        Periodically refresh finger table entries
+        Get the predecessor of this current node.
 
         Args:
             N/A
+
+        Returns:
+            The id of the predecessor.
+
+        Raises:
+            N/A
+        '''
+        return self._predecessor
+
+    def remote_set_predecessor(self, remote_node, identity):
+        '''
+        Set the predecessor of the remote_node.
+
+        Args:
+            remote_node:    The remote node id.
+            identity:       The new predecessor identity.
+
+        Returns:
+            The id of the predecessor.
+
+        Raises:
+            N/A
+        '''
+        # TODO
+        pass
+
+    def set_predecessor(self, identity):
+        '''
+        Set the precessor of this current node.
+
+        Args:
+            identity:   The identity of the predecessor.
 
         Returns:
             N/A
@@ -199,5 +252,53 @@ class Node(object):
         Raises:
             N/A
         '''
-        # TODO
-        pass
+        self._predecessor = identity
+    
+    # Advanced
+    # def stablize(self):
+        # '''
+        # Periodically verify n’s immediate successor, and tell the successor about n
+
+        # Args:
+            # N/A
+
+        # Returns:
+            # N/A
+
+        # Raises:
+            # N/A
+        # '''
+        # # TODO
+        # pass
+
+    # def notify(self, remote_node):
+        # '''
+        # The remote node thinks it might be our predecessor.
+
+        # Args:
+            # remote_node:   the remote node notifying this node.
+
+        # Returns:
+            # N/A
+
+        # Raises:
+            # N/A
+        # '''
+        # # TODO
+        # pass
+
+    # def fix_fingers(self):
+        # '''
+        # Periodically refresh finger table entries.
+
+        # Args:
+            # N/A
+
+        # Returns:
+            # N/A
+
+        # Raises:
+            # N/A
+        # '''
+        # # TODO
+        # pass
