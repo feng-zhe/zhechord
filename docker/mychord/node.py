4e902c37719ef20eb511c65177eea6f77f5198e2
Henry Feng <henryzhefeng@gmail.com>
2018-03-17 16:29:44 -0400

    re-org

--- docker/mychord/node.py
+++ docker/mychord/node.py
@@ -117,6 +117,120 @@ class Node(object):
             for i in range(1, ct.RING_SIZE_BIT+1):
                 self._table.set_node(1, self._id)
 
+    def init_finger_table(self, remote_node):
+        '''
+        Initialize the calling node's finger table by the remote_node.
+
+        Args:
+            remote_node:    The remote node id.
+
+        Returns:
+            N/A
+
+        Raises:
+            N/A
+        '''
+        successor = self.remote_find_successor(remote_node, 
+                                self._table.get_start(1))
+        self._table.set_node(1, successor)
+        self._predecessor = self.remote_get_predecessor(successor)
+        self.remote_set_predecessor(successor, self._id)
+        for i in range(1, ct.RING_SIZE_BIT):
+            start = self._table.get_start(i+1)
+            fnode = self._table.get_node(i)
+            if self._in_range(start, self._id, fnode):
+                self._table.set_node(i+1, fnode)
+            else:
+                remote_succ = self.remote_find_successor(remote_node, start)
+                self._table.set_node(i+1, remote_succ)
+            
+    def update_others(self):
+        '''
+        Update all nodes whose finger tables should refer to n
+
+        Args:
+            N/A
+
+        Returns:
+            N/A
+
+        Raises:
+            N/A
+        '''
+        # TODO
+        pass
+
+    def update_finger_table(self, s, i):
+        '''
+        If s is i^th finger of n, update n’s finger table with s
+        TODO
+        '''
+        # TODO
+        pass
+
+    def get_predecessor(self):
+        '''
+        Get the predecessor of this current node.
+
+        Args:
+            N/A
+
+        Returns:
+            The id of the predecessor.
+
+        Raises:
+            N/A
+        '''
+        return self._predecessor
+
+    def set_predecessor(self, identity):
+        '''
+        Set the precessor of this current node.
+
+        Args:
+            identity:   The identity of the predecessor.
+
+        Returns:
+            N/A
+
+        Raises:
+            N/A
+        '''
+        self._predecessor = identity
+
+    def remote_get_predecessor(self, remote_node):
+        '''
+        Get the predecessor of the remote_node.
+
+        Args:
+            remote_node:    The remote node id.
+
+        Returns:
+            The id of the predecessor.
+
+        Raises:
+            N/A
+        '''
+        # TODO
+        pass
+
+    def remote_set_predecessor(self, remote_node, identity):
+        '''
+        Set the predecessor of the remote_node.
+
+        Args:
+            remote_node:    The remote node id.
+            identity:       The new predecessor identity.
+
+        Returns:
+            The id of the predecessor.
+
+        Raises:
+            N/A
+        '''
+        # TODO
+        pass
+    
     def remote_find_successor(self, remote_node, identity):
         '''
         Ask the remote node to find the successor of identity
@@ -151,33 +265,6 @@ class Node(object):
         # TODO
         pass
 
-    def init_finger_table(self, remote_node):
-        '''
-        Initialize the calling node's finger table by the remote_node.
-
-        Args:
-            remote_node:    The remote node id.
-
-        Returns:
-            N/A
-
-        Raises:
-            N/A
-        '''
-        successor = self.remote_find_successor(remote_node, 
-                                self._table.get_start(1))
-        self._table.set_node(1, successor)
-        self._predecessor = self.remote_get_predecessor(successor)
-        self.remote_set_predecessor(successor, self._id)
-        for i in range(1, ct.RING_SIZE_BIT):
-            start = self._table.get_start(i+1)
-            fnode = self._table.get_node(i)
-            if self._in_range(start, self._id, fnode):
-                self._table.set_node(i+1, fnode)
-            else:
-                remote_succ = self.remote_find_successor(remote_node, start)
-                self._table.set_node(i+1, remote_succ)
-            
     def _in_range(self, node, start, end):
         '''
         Test whether the node is in [start, end).
@@ -197,93 +284,6 @@ class Node(object):
         # TODO
         pass
 
-    def update_others(self):
-        '''
-        Update all nodes whose finger tables should refer to n
-
-        Args:
-            N/A
-
-        Returns:
-            N/A
-
-        Raises:
-            N/A
-        '''
-        # TODO
-        pass
-
-    def update_finger_table(self, s, i):
-        '''
-        If s is i^th finger of n, update n’s finger table with s
-        TODO
-        '''
-        # TODO
-        pass
-
-    def remote_get_predecessor(self, remote_node):
-        '''
-        Get the predecessor of the remote_node.
-
-        Args:
-            remote_node:    The remote node id.
-
-        Returns:
-            The id of the predecessor.
-
-        Raises:
-            N/A
-        '''
-        # TODO
-        pass
-
-    def get_predecessor(self):
-        '''
-        Get the predecessor of this current node.
-
-        Args:
-            N/A
-
-        Returns:
-            The id of the predecessor.
-
-        Raises:
-            N/A
-        '''
-        return self._predecessor
-
-    def remote_set_predecessor(self, remote_node, identity):
-        '''
-        Set the predecessor of the remote_node.
-
-        Args:
-            remote_node:    The remote node id.
-            identity:       The new predecessor identity.
-
-        Returns:
-            The id of the predecessor.
-
-        Raises:
-            N/A
-        '''
-        # TODO
-        pass
-
-    def set_predecessor(self, identity):
-        '''
-        Set the precessor of this current node.
-
-        Args:
-            identity:   The identity of the predecessor.
-
-        Returns:
-            N/A
-
-        Raises:
-            N/A
-        '''
-        self._predecessor = identity
-    
     # Advanced
     # def stablize(self):
         # '''
