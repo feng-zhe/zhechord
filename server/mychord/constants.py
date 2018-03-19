ea0d92d8ee6466f94b14e9fd5e02188a14665488
Henry Feng <henryzhefeng@gmail.com>
2018-03-19 13:37:03 -0400

    fix hardcode init issue

--- server/mychord/constants.py
+++ server/mychord/constants.py
@@ -7,8 +7,8 @@ ID_MAX = 2**RING_SIZE_BIT
 TWO_EXP = []  # the 2^i table, to speed up
 
 def init():
-    if len(TWO_EXP) == 0:
-        for i in range(0, RING_SIZE_BIT+1):
-            TWO_EXP.append(2**i)
+    ID_MAX = 2**RING_SIZE_BIT
+    for i in range(0, RING_SIZE_BIT+1):
+        TWO_EXP.append(2**i)
 
 init()
