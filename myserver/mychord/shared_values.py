5ead8daf3e1dbe2e517ae715e3bf925f76e4a3d6
Henry Feng <henryzhefeng@gmail.com>
2018-03-26 22:40:20 -0400

    add code to call periodical functions

--- myserver/mychord/shared_values.py
+++ myserver/mychord/shared_values.py
@@ -1,12 +1,26 @@
 '''
 Shared values among files.
 '''
+import threading
+import time
 import mychord.node as node
 
 # shared values
 g_node = None
 
+def period():
+    '''
+    Periodically call the stabilize and fix_finger_table
+    '''
+    while True:
+        g_node.stabilize()
+        g_node.fix_fingers()
+        time.sleep(1)
+
 def init(self_id, remote_id=None):
     global g_node 
     g_node = node.Node(self_id)
     g_node.join(remote_id)
+    t = threading.Thread(target=period)
+    t.daemon = True
+    t.start()
