4de40168e8b53c91f3cc78b586d5859e8a78ca56
Henry Feng <henryzhefeng@gmail.com>
2018-04-16 23:46:17 -0400

    rename files

--- myserver/tests/test_node_5.py
+++ myserver/tests/test_node_5.py
@@ -1,5 +1,5 @@
 '''
-This file tests the core functionality of Node on a ring of 2^3=8 nodes.
+This file tests the core functionality of Node on a ring of 2^5=32 nodes.
 '''
 import unittest
 import logging
@@ -27,7 +27,7 @@ class TestNodeCore(unittest.TestCase):
         Change to smaller ring.
         '''
         cls._default_size = ct.RING_SIZE_BIT
-        ct.RING_SIZE_BIT = 3
+        ct.RING_SIZE_BIT = 5
         ct.init()
 
     @classmethod
@@ -39,210 +39,78 @@ class TestNodeCore(unittest.TestCase):
         ct.init()
 
     @patch('requests.post')
-    def test_join_0_3_1_6(self, post_mock):
+    def test_join_0_1_3_11_15_1c(self, post_mock):
         '''
-        Use the example in paper.
-        '''
-        # set up mock server
-        ms = MockServer()
-        post_mock.side_effect = lambda url, json, timeout : ms.post(url, json)
-        # test 1: node 0 creates a new ring
-        node_0 = Node('0')
-        ms.add_node(node_0._id, node_0)       # add this node to mocked server
-        node_0.join()
-        for i in range(1, ct.RING_SIZE_BIT+1):      # should point to itself
-            self.assertEqual(
-                    node_0._table.get_node(i), 
-                    node_0._id)
-        # test 2: join node 3 to then ring
-        node_3 = Node('3')
-        ms.add_node(node_3._id, node_3)
-        node_3.join(node_0._id)
-        for i in range(0, 10):      # imitate the periodic operations
-            ms.period()
-        self.assertEqual(node_3._table.get_node(1), node_0._id) # node 3 status
-        self.assertEqual(node_3._table.get_node(2), node_0._id)
-        self.assertEqual(node_3._table.get_node(3), node_0._id)
-        self.assertEqual(node_3.get_predecessor(), node_0._id)
-        self.assertEqual(node_0._table.get_node(1), node_3._id) # node 0 status
-        self.assertEqual(node_0._table.get_node(2), node_3._id)
-        self.assertEqual(node_0._table.get_node(3), node_0._id)
-        self.assertEqual(node_0.get_predecessor(), node_3._id)
-        # test 3: join node 1 to the ring
-        node_1 = Node('1')
-        ms.add_node(node_1._id, node_1)
-        node_1.join(node_3._id)
-        for i in range(0, 10):      # imitate the periodic operations
-            ms.period()
-        self.assertEqual(node_1._table.get_node(1), node_3._id) # node 1 status
-        self.assertEqual(node_1._table.get_node(2), node_3._id)
-        self.assertEqual(node_1._table.get_node(3), node_0._id)
-        self.assertEqual(node_1.get_predecessor(), node_0._id)
-        self.assertEqual(node_3._table.get_node(1), node_0._id) # node 3 status
-        self.assertEqual(node_3._table.get_node(2), node_0._id)
-        self.assertEqual(node_3._table.get_node(3), node_0._id)
-        self.assertEqual(node_3.get_predecessor(), node_1._id)
-        self.assertEqual(node_0._table.get_node(1), node_1._id) # node 0 status
-        self.assertEqual(node_0._table.get_node(2), node_3._id)
-        self.assertEqual(node_0._table.get_node(3), node_0._id)
-        self.assertEqual(node_0.get_predecessor(), node_3._id)
-        # test 4: join node 6 to the ring
-        node_6 = Node('6')
-        ms.add_node(node_6._id, node_6)
-        node_6.join(node_1._id)
-        for i in range(0, 10):      # imitate the periodic operations
-            ms.period()
-        self.assertEqual(node_6._table.get_node(1), node_0._id) # node 6 status
-        self.assertEqual(node_6._table.get_node(2), node_0._id)
-        self.assertEqual(node_6._table.get_node(3), node_3._id)
-        self.assertEqual(node_6.get_predecessor(), node_3._id)
-        self.assertEqual(node_1._table.get_node(1), node_3._id) # node 1 status
-        self.assertEqual(node_1._table.get_node(2), node_3._id)
-        self.assertEqual(node_1._table.get_node(3), node_6._id)
-        self.assertEqual(node_1.get_predecessor(), node_0._id)
-        self.assertEqual(node_3._table.get_node(1), node_6._id) # node 3 status
-        self.assertEqual(node_3._table.get_node(2), node_6._id)
-        self.assertEqual(node_3._table.get_node(3), node_0._id)
-        self.assertEqual(node_3.get_predecessor(), node_1._id)
-        self.assertEqual(node_0._table.get_node(1), node_1._id) # node 0 status
-        self.assertEqual(node_0._table.get_node(2), node_3._id)
-        self.assertEqual(node_0._table.get_node(3), node_6._id)
-        self.assertEqual(node_0.get_predecessor(), node_6._id)
-
-    @patch('requests.post')
-    def test_join_6_1_3_0(self, post_mock):
-        '''
-        Use the example in paper.
+        Demo example for 32-node ring.
+        Only test the final result.
         '''
         # set up mock server
         ms = MockServer()
         post_mock.side_effect = lambda url, json, timeout : ms.post(url, json)
-        # test 1: node 6 creates a new ring
-        node_6 = Node('6')
-        ms.add_node(node_6._id, node_6)       # add this node to mocked server
-        node_6.join()
-        for i in range(1, ct.RING_SIZE_BIT+1):      # should point to itself
-            self.assertEqual(
-                    node_6._table.get_node(i), 
-                    node_6._id)
-        # test 2: join node 1 to then ring
-        node_1 = Node('1')
-        ms.add_node(node_1._id, node_1)
-        node_1.join(node_6._id)
-        for i in range(0, 10):      # imitate the periodic operations
-            ms.period()
-        self.assertEqual(node_1._table.get_node(1), node_6._id) # node 1 status
-        self.assertEqual(node_1._table.get_node(2), node_6._id)
-        self.assertEqual(node_1._table.get_node(3), node_6._id)
-        self.assertEqual(node_1.get_predecessor(), node_6._id)
-        self.assertEqual(node_6._table.get_node(1), node_1._id) # node 6 status
-        self.assertEqual(node_6._table.get_node(2), node_1._id)
-        self.assertEqual(node_6._table.get_node(3), node_6._id)
-        self.assertEqual(node_6.get_predecessor(), node_1._id)
-        # test 3: join node 3 to the ring
-        node_3 = Node('3')
-        ms.add_node(node_3._id, node_3)
-        node_3.join(node_1._id)
-        for i in range(0, 10):      # imitate the periodic operations
-            ms.period()
-        self.assertEqual(node_3._table.get_node(1), node_6._id) # node 3 status
-        self.assertEqual(node_3._table.get_node(2), node_6._id)
-        self.assertEqual(node_3._table.get_node(3), node_1._id)
-        self.assertEqual(node_3.get_predecessor(), node_1._id)
-        self.assertEqual(node_1._table.get_node(1), node_3._id) # node 1 status
-        self.assertEqual(node_1._table.get_node(2), node_3._id)
-        self.assertEqual(node_1._table.get_node(3), node_6._id)
-        self.assertEqual(node_1.get_predecessor(), node_6._id)
-        self.assertEqual(node_6._table.get_node(1), node_1._id) # node 6 status
-        self.assertEqual(node_6._table.get_node(2), node_1._id)
-        self.assertEqual(node_6._table.get_node(3), node_3._id)
-        self.assertEqual(node_6.get_predecessor(), node_3._id)
-        # test 4: join node 0 to the ring
-        node_0 = Node('0')
-        ms.add_node(node_0._id, node_0)
-        node_0.join(node_3._id)
-        for i in range(0, 10):      # imitate the periodic operations
-            ms.period()
-        self.assertEqual(node_0._table.get_node(1), node_1._id) # node 0 status
-        self.assertEqual(node_0._table.get_node(2), node_3._id)
-        self.assertEqual(node_0._table.get_node(3), node_6._id)
-        self.assertEqual(node_0.get_predecessor(), node_6._id)
-        self.assertEqual(node_3._table.get_node(1), node_6._id) # node 3 status
-        self.assertEqual(node_3._table.get_node(2), node_6._id)
-        self.assertEqual(node_3._table.get_node(3), node_0._id)
-        self.assertEqual(node_3.get_predecessor(), node_1._id)
-        self.assertEqual(node_1._table.get_node(1), node_3._id) # node 1 status
-        self.assertEqual(node_1._table.get_node(2), node_3._id)
-        self.assertEqual(node_1._table.get_node(3), node_6._id)
-        self.assertEqual(node_1.get_predecessor(), node_0._id)
-        self.assertEqual(node_6._table.get_node(1), node_0._id) # node 6 status
-        self.assertEqual(node_6._table.get_node(2), node_0._id)
-        self.assertEqual(node_6._table.get_node(3), node_3._id)
-        self.assertEqual(node_6.get_predecessor(), node_3._id)
-
-    @patch('requests.post')
-    def test_display_finger_table(self, post_mock):
-        # set up mock server
-        ms = MockServer()
-        post_mock.side_effect = lambda url, json, timeout : ms.post(url, json)
-        # test:  join 3 node
-        logging.disable(logging.DEBUG)      # disable logging
-        node_0 = Node('0')
-        ms.add_node(node_0._id, node_0)
-        node_0.join()
-        node_3 = Node('3')
-        ms.add_node(node_3._id, node_3)
-        node_3.join(node_0._id)
+        # node 0
+        node_00 = Node('00')
+        ms.add_node(node_00._id, node_00)       # add this node to mocked server
+        node_00.join()
+        # node 1
+        node_01 = Node('01')
+        ms.add_node(node_01._id, node_01)
+        node_01.join(node_00._id)
+        # node 3
+        node_03 = Node('03')
+        ms.add_node(node_03._id, node_03)
+        node_03.join(node_01._id)
+        # node 11 (17 in decimal)
+        node_11 = Node('11')
+        ms.add_node(node_11._id, node_11)
+        node_11.join(node_03._id)
+        # node 15 (21 in decimal)
+        node_15 = Node('15')
+        ms.add_node(node_15._id, node_15)
+        node_15.join(node_11._id)
+        # node 1c (29 in decimal)
+        node_1c = Node('1c')
+        ms.add_node(node_1c._id, node_1c)
+        node_1c.join(node_15._id)
+        # imitate the periodic operations
         for i in range(0, 10):
             ms.period()
-        node_1 = Node('1')
-        ms.add_node(node_1._id, node_1)
-        node_1.join(node_3._id)
-        for i in range(0, 10):
-            ms.period()
-        logging.disable(logging.NOTSET)      # enable logging
-        ft = node_0.display_finger_table()
-        self.assertEqual(ft[0], '3')
-        self.assertEqual(ft[1], '1')
-        self.assertEqual(ft[2], '3')
-        self.assertEqual(ft[3], '0')
-        ft = node_3.display_finger_table()
-        self.assertEqual(ft[0], '1')
-        self.assertEqual(ft[1], '0')
-        self.assertEqual(ft[2], '0')
-        self.assertEqual(ft[3], '0')
-        ft = node_1.display_finger_table()
-        self.assertEqual(ft[0], '0')
-        self.assertEqual(ft[1], '3')
-        self.assertEqual(ft[2], '3')
-        self.assertEqual(ft[3], '0')
-
-    @patch('requests.post')
-    def test_put_get(self, post_mock):
-        # set up mock server
-        ms = MockServer()
-        post_mock.side_effect = lambda url, json, timeout : ms.post(url, json)
-        # create nodes
-        logging.disable(logging.DEBUG)      # disable logging
-        node_0 = Node('0')
-        ms.add_node(node_0._id, node_0)
-        node_0.join()
-        node_3 = Node('3')
-        ms.add_node(node_3._id, node_3)
-        node_3.join(node_0._id)
-        for i in range(0, 10):
-            ms.period()
-        node_1 = Node('1')
-        ms.add_node(node_1._id, node_1)
-        node_1.join(node_3._id)
-        for i in range(0, 10):
-            ms.period()
-        logging.disable(logging.NOTSET)      # enable logging
-        # put&get key-value
-        node_0.local_put('hello', 'world')
-        self.assertEqual(node_0.local_get('hello'), 'world')
-        self.assertEqual(node_3.local_get('hello'), None)
-        self.assertEqual(node_1.local_get('hello'), None)
+        # test values
+        self.assertEqual(node_00.get_predecessor(), node_1c._id) # node 0 status
+        self.assertEqual(node_00._table.get_node(1), node_01._id)
+        self.assertEqual(node_00._table.get_node(2), node_03._id)
+        self.assertEqual(node_00._table.get_node(3), node_11._id)
+        self.assertEqual(node_00._table.get_node(4), node_11._id)
+        self.assertEqual(node_00._table.get_node(5), node_11._id)
+        self.assertEqual(node_01.get_predecessor(), node_00._id) # node 1 status
+        self.assertEqual(node_01._table.get_node(1), node_03._id)
+        self.assertEqual(node_01._table.get_node(2), node_03._id)
+        self.assertEqual(node_01._table.get_node(3), node_11._id)
+        self.assertEqual(node_01._table.get_node(4), node_11._id)
+        self.assertEqual(node_01._table.get_node(5), node_11._id)
+        self.assertEqual(node_03.get_predecessor(), node_01._id) # node 3 status
+        self.assertEqual(node_03._table.get_node(1), node_11._id)
+        self.assertEqual(node_03._table.get_node(2), node_11._id)
+        self.assertEqual(node_03._table.get_node(3), node_11._id)
+        self.assertEqual(node_03._table.get_node(4), node_11._id)
+        self.assertEqual(node_03._table.get_node(5), node_15._id)
+        self.assertEqual(node_11.get_predecessor(), node_03._id) # node 11 status
+        self.assertEqual(node_11._table.get_node(1), node_15._id)
+        self.assertEqual(node_11._table.get_node(2), node_15._id)
+        self.assertEqual(node_11._table.get_node(3), node_15._id)
+        self.assertEqual(node_11._table.get_node(4), node_1c._id)
+        self.assertEqual(node_11._table.get_node(5), node_01._id)
+        self.assertEqual(node_15.get_predecessor(), node_11._id) # node 15 status
+        self.assertEqual(node_15._table.get_node(1), node_1c._id)
+        self.assertEqual(node_15._table.get_node(2), node_1c._id)
+        self.assertEqual(node_15._table.get_node(3), node_1c._id)
+        self.assertEqual(node_15._table.get_node(4), node_00._id)
+        self.assertEqual(node_15._table.get_node(5), node_11._id)
+        self.assertEqual(node_1c.get_predecessor(), node_15._id) # node 1c status
+        self.assertEqual(node_1c._table.get_node(1), node_00._id)
+        self.assertEqual(node_1c._table.get_node(2), node_00._id)
+        self.assertEqual(node_1c._table.get_node(3), node_00._id)
+        self.assertEqual(node_1c._table.get_node(4), node_11._id)
+        self.assertEqual(node_1c._table.get_node(5), node_11._id)
 
 if __name__ == '__main__':
     unittest.main()
